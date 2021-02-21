f9188df8bd9e2ccdb8487dbff2fb154e48b3f3ff
Phu Nguyen <phu21994@gmail.com>
2021-02-21 20:17:44 +0700

    Fix cors to cors()

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ import Content from './dbquote.js'
 import cors from "cors"
 //app config
 const app = express();
-const port = process.env.PORT || 52857
+const port = process.env.PORT || 9000
 
 //middleware
-app.use(cors)
+
 app.use(express.json());
+app.use(cors());
 
 
 //DB config
@@ -17,37 +18,39 @@ const connect_URL = "mongodb+srv://phu1994:6298327@cluster0.rpsig.mongodb.net/Ra
 mongoose.connect(connect_URL, { useNewUrlParser: true,  useUnifiedTopology: true });
 
 //api routes
-app.get("/", (req, res)=> res.status(200).send("hello world"));
-app.get("/content/quote", async function(req, res) {
+app.get("/", function(req, res) {
+    res.status(200).send("hello world");
+
+})
+app.get("/content/quote", async function getAllContent(req, res) {
     const content = await Content.find();
 	res.json(content)
 });
 
-app.post("/content/id", (req, res) =>{
+app.post("/content/id", async function postOneContent(req, res) {
     const quote = req.body;
-    Content.create(quote, (err, data) => {
-        if (err) {
-            res.status(500).send('err')
-        } else {
-            res.status(201).send(`New content was created: ${data}`)
-        }
-    
+    const data = await Content.create(quote);  
+    if (!data) {
+        res.status(500).send('err')
+    } else {
+        res.status(201).send(`New content was created: ${data}`)
+    }
+
     })
 
-})
 
-app.post("/", (req, res) =>{
-    const quote = req.body;
-    Content.create(quote, (err, data) => {
-        if (err) {
-            res.status(500).send('err')
-        } else {
-            res.status(201).send(`New content was created: ${data}`)
-        }
+// app.post("/", async function postAllContent(req, res) {
+//     const quote = req.body;
+//     Content.create(quote, (err, data) => {
+//         if (err) {
+//             res.status(500).send('err')
+//         } else {
+//             res.status(201).send(`New content was created: ${data}`)
+//         }
     
-    })
+//     })
 
-})
+// })
 
 // app.get("/content/id", (req, res) => {
 //     const id =  req.body
